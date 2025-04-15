3636443de0a97885c42cf6d59d05be6cf457ee93
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2025-04-15 12:01:26 +0300

    Initial commit

---
+++ src/main/java/com/edwin/slotify/SlotifyApplication.java
@@ -0,0 +1,13 @@
+package com.edwin.slotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edwin/slotify/SlotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.slotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
