cac2f98b8bc37139e5d5f0989451aa7fb2f44e89
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2025-04-18 09:39:34 +0300

    mongodb no longer in use

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,10 +25,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("me.paulschwarz:spring-dotenv:4.0.0")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
